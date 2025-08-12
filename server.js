51e50e8b21f1e0d56c2a9b69b0178a86459a233c
Sandip-droid <chakrabortysandipan862@gmail.com>
2025-08-12 21:18:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const connectDb = require('./config/mongodb'); // Make sure this path is correct
+const connectCloudinary=require('./config/clodinary')
+
+
+const adminRoutes = require('./routes/adminRoutes'); // Make sure this path is correct
+// --- APP CONFIG ---
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/api',adminRoutes)
+
+//localhost
+
+const PORT = process.env.PORT || 4000;
+
+// Call the function to connect to the database after dotenv has loaded.
+connectDb();
+connectCloudinary();
+
+// --- ROUTES ---
+app.get('/', (req, res) => {
+    console.log("API is working");
+    res.send("API is working");
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
